4cd05b0c117611905db00224205aa8ea56b3aba8
MOHSIN-ALi <153511839+Mohsin-surhio@users.noreply.github.com>
2024-03-06 11:15:27 -0800

    Add files via upload

---
+++ Governor IT/Assignment 1/main.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+var Fullname = "Mohsin ali age";
+var number = 18;
+console.log("".concat(Fullname, ",").concat(number));
---
+++ Governor IT/Assignment 1/main.ts
@@ -0,0 +1,5 @@
+console.log(`Hello World`);
+
+let Fullname ="Mohsin ali age";
+let number = 18
+console.log (`${Fullname},${number}`);
